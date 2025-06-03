ba79c06df2f2e9e1bdbe993c7adb56ebb60112d6
Sakar Bhandari <bhandarisakar@outlook.com>
2025-06-03 14:36:39 +0545

    MEDIA FILES

--- ch4-bookstore/django_project/urls.py
+++ ch4-bookstore/django_project/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path("",include("pages.urls")),  # Include URLs from the pages app
     path("books/", include("books.urls")),  # Include URLs from the books app
     
-]
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(
+settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
